a295ca133bda8d8ac8514cc45052a990b183238d
Andrew Davey <ajdavey8@gmail.com>
2020-04-24 14:03:44 +0100

    :tada: Intitial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+const Text = styled.p`
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+const App = () => (
+  <div>
+    <Text>ToDo List</Text>
+  </div>
+);
+
+export default App;
